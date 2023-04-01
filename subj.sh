966e92a581934bee7065195b6e37fdb4f896ee8a
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-04-01 22:26:49 +0300

    Fix bugs

--- subj.sh
+++ subj.sh
@@ -4,6 +4,6 @@ git clone "$url" "./$1/LaTex"
 
 subjectpath="data/subject.txt"
 
-if [[ $# -ge 1 ]]; then
-	echo $(tr $2 -d '\n') >> "./$1/LaTex/$subjectpath"
+if [[ $# -ge 2 ]]; then
+	echo $2 >> "./$1/LaTex/$subjectpath"
 fi
