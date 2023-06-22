142f784f249a87b84499594fe9a9f36200fc06d4
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-06-22 14:39:48 +0300

    Rename LaTex to LaTeX

--- subj.sh
+++ subj.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 url=https://github.com/Dolfost/NAU-LaTex-Template
-git clone "$url" "./$1/LaTex"
+git clone "$url" "./$1/LaTeX"
 
 subjectpath="data/subject.txt"
 
 if [[ $# -ge 2 ]]; then
-	echo $2 > "./$1/LaTex/$subjectpath"
+	echo $2 > "./$1/LaTeX/$subjectpath"
 fi
 
-cd "./$1/LaTex"
+cd "./$1/LaTeX"
