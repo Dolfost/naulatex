601c664a305d52890238b5645f89350a015889ba
Vladyslav Rehan <vladyslav@dmac.local>
2024-12-09 14:20:29 +0200

    Restructure more

--- src/cmake/setup.cmake
+++ src/cmake/setup.cmake
@@ -1,47 +1,44 @@
 function(naulatex_setup SUBJECT JOB)
-	set(LOCK "${NAULATEX_CONTENTS_DIRECTORY}/configured.lock" )
+	set(CHAPTERS_LOCK "${NAULATEX_CONTENTS_DIRECTORY}/chapters.lock" )
 
 	message(STATUS "Configuring latex template...")
-	macro(show_settings)
-		message(STATUS "Selected subject:job => ${SUBJECT}:${JOB}")
+	macro(show_settings PREF)
+		message(STATUS "${PREF} subject:job => ${SUBJECT}:${JOB}")
 	endmacro()
-	show_settings()
-if (EXISTS "${LOCK}")
-		message(STATUS "The lockfile already exists, "
-			"subject have been already selected and configured"
+	show_settings("Settings from options:")
+	if (EXISTS "${CHAPTERS_LOCK}")
+		message(STATUS "Chpaters lockfile already exists, "
+			"chapters have been already selected and configured"
 		)
-		file(STRINGS "${LOCK}" lock) 
+		file(STRINGS "${CHAPTERS_LOCK}" lock) 
 		list(GET lock 0 SUBJECT)
 		list(GET lock 1 JOB)
-		message(STATUS "Settings from lockfile:")
-		show_settings()
+		show_settings("Settings from chapters lockfile:")
 	else()
 		message(STATUS "The lockfile doesn't exists, "
-			"setting NAULATEX_OVERRIDE to YES"
+			"setting NAULATEX_OVERRIDE_CHAPTERS to YES"
 		)
-		set(NAULATEX_OVERRIDE YES)
+		set(NAULATEX_OVERRIDE_CHAPTERS YES)
 	endif()
 
 	set(SUBJECT_PATH "${NAULATEX_TEMPLATES_DIRECTORY}/subjects/${SUBJECT}")
 	set(JOB_PATH "${NAULATEX_TEMPLATES_DIRECTORY}/jobs/${JOB}")
 
-	if(DEFINED NAULATEX_OVERRIDE)
-		if(NAULATEX_OVERRIDE)
-			message(STATUS "Because NAULATEX_OVERRIDE=YES, "
-				"current project will be reconfigured"
+	if(DEFINED NAULATEX_OVERRIDE_CHAPTERS)
+		if(NAULATEX_OVERRIDE_CHAPTERS)
+			message(STATUS "Because NAULATEX_OVERRIDE_CHAPTERS=YES, "
+				"chapters will be reconfigured"
 			)
-			set(RECONFIGURE YES)
 		else()
-			message(STATUS "Because NAULATEX_OVERRIDE=NO, "
+			message(STATUS "Because NAULATEX_OVERRIDE_CHAPTERS=NO, "
 				"project chapters won't be reconfigured"
 			)
-			set(RECONFIGURE NO)
 		endif()
 	else()
-		message(STATUS "If you want to reconfigure contents/, "
-			"configure with -DNAULATEX_OVERRIDE=YES"
+		message(STATUS "If you want to reconfigure contents/chapters, "
+			"configure with -DNAULATEX_OVERRIDE_CHAPTERS=YES"
 		)
-		set(RECONFIGURE NO)
+		set(NAULATEX_OVERRIDE_CHAPTERS NO)
 	endif()
 
 	if (NOT EXISTS ${SUBJECT_PATH})
@@ -54,44 +51,56 @@ if (EXISTS "${LOCK}")
 		)
 	endif()
 
-	if(RECONFIGURE)
+	if(${NAULATEX_OVERRIDE_CHAPTERS})
 		message(STATUS "Setting up chapters")
 		file(REMOVE_RECURSE "${NAULATEX_CONTENTS_DIRECTORY}/chapters")
 		file(COPY "${SUBJECT_PATH}/chapters"
 			DESTINATION "${NAULATEX_CONTENTS_DIRECTORY}"
 		)
+		configure_file("${SUBJECT_PATH}/chapters.tex"
+			"${NAULATEX_CONFIG_DIRECTORY}/chapters.tex"
+			COPYONLY
+		)
 		file(CONFIGURE
-			OUTPUT "${NAULATEX_CONTENTS_DIRECTORY}/configured.lock"
+			OUTPUT "${CHAPTERS_LOCK}"
 			@ONLY CONTENT "@SUBJECT@\n@JOB@"
 		)
 	else()
 		message(STATUS "Setting up chapters - skipped")
 	endif()
 
-	message(STATUS "Setting up chapters header")
-	configure_file(${SUBJECT_PATH}/chapters.tex
-		${NAULATEX_CONFIG_DIRECTORY}/chapters.tex
-		COPYONLY
-	)
+	if (NOT EXISTS "${NAULATEX_CONTENTS_DIRECTORY}/redefines.tex")
+		message(STATUS "Setting up contents redefines")
+		configure_file("${NAULATEX_TEMPLATES_DIRECTORY}/redefines.tex.in"
+			"${NAULATEX_CONTENTS_DIRECTORY}/redefines.tex"
+			@ONLY
+		)
+	else()
+		message(STATUS "Setting up contents redefines - skipped (already exists)")
+	endif()
+
+	if (NOT EXISTS "${NAULATEX_CONTENTS_DIRECTORY}/bibliography.tex")
+		message(STATUS "Setting up contents bibliography")
+		file(TOUCH "${NAULATEX_CONTENTS_DIRECTORY}/bibliography.bib")
+	else()
+		message(STATUS "Setting up contents bibliography - skipped (already exists)")
+	endif()
+
 	message(STATUS "Setting up subject defines")
 	configure_file(${SUBJECT_PATH}/defines.tex
 		${NAULATEX_CONFIG_DIRECTORY}/subjectDefines.tex
 		COPYONLY
 	)
-
 	message(STATUS "Setting up job defines")
 	configure_file(${JOB_PATH}/defines.tex
 		${NAULATEX_CONFIG_DIRECTORY}/jobDefines.tex
 		COPYONLY
 	)
-
 	message(STATUS "Setting up bibliography")
 	configure_file(${SUBJECT_PATH}/bibliography.bib
 		${NAULATEX_CONFIG_DIRECTORY}/bibliography.bib
 		COPYONLY
 	)
-	file(TOUCH "${NAULATEX_CONTENTS_DIRECTORY}/bibliography.bib")
-
 	file(MAKE_DIRECTORY ${NAULATEX_CONFIG_DIRECTORY}/matter)
 	set(MATTER title frontmatter backmatter)
 	foreach(matter ${MATTER})
@@ -110,7 +119,7 @@ if (EXISTS "${LOCK}")
 		endif()
 	endforeach()
 	
-	set(NAULATEX_OVERRIDE OFF CACHE BOOL 
-		"Trigger chapters overriding" FORCE
+	set(NAULATEX_OVERRIDE_CHAPTERS OFF CACHE BOOL
+		"Set chapters overriding" FORCE
 	)
 endfunction()
